b9ca24c967f9741e56098971571c79ef845daccf
Adam Blaszkiewicz <atablash@atablash.pl>
2017-11-13 22:21:53 +0100

    updated kd tests to 4 dimensions for checking eigen aligned memory

--- test/test-kd.cpp
+++ test/test-kd.cpp
@@ -7,20 +7,22 @@ using namespace ab;
 
 
 
-using Vec3 = Eigen::Matrix<double, 3, 1>;
-using Vec3_noalign = Eigen::Matrix<double, 3, 1, Eigen::DontAlign>;
+using Vec4 = Eigen::Matrix<double, 4, 1>;
+using Vec4_noalign = Eigen::Matrix<double, 4, 1, Eigen::DontAlign>;
 
 
 
 template<class KD>
 void insert_grid(KD& kd) {
 
-	std::vector<Vec3_noalign> v;
+	std::vector<Vec4_noalign> v;
 
 	for(int x=0; x<10; ++x) {
 		for(int y=0; y<10; ++y) {
 			for(int z=0; z<10; ++z) {
-				v.emplace_back(x,y,z);
+				for(int w=0; w<10; ++w) {
+					v.emplace_back(x,y,z,w);
+				}
 			}
 		}
 	}
@@ -35,12 +37,14 @@ void insert_grid(KD& kd) {
 template<class KD>
 void insert_grid_keyval(KD& kd) {
 
-	std::vector<std::pair<Vec3_noalign,int>> v;
+	std::vector<std::pair<Vec4_noalign,int>> v;
 
-	for(int x=0; x<10; ++x) {
-		for(int y=0; y<10; ++y) {
-			for(int z=0; z<10; ++z) {
-				v.emplace_back(Vec3_noalign{1.0*x, 1.0*y, 1.0*z}, 100*x + 10*y + z);
+	for(int x=0; x<5; ++x) {
+		for(int y=0; y<5; ++y) {
+			for(int z=0; z<5; ++z) {
+				for(int w=0; w<5; ++w) {
+					v.emplace_back(Vec4_noalign{1.0*x, 1.0*y, 1.0*z, 1.0*w}, 1000*x + 100*y + 10*z + w);
+				}
 			}
 		}
 	}
@@ -54,101 +58,104 @@ void insert_grid_keyval(KD& kd) {
 
 
 TEST(Kd, FindClosest) {
-	Kd<double, 3> kd;
+	Kd<double, 4> kd;
 
 	insert_grid(kd);
 
-	auto v = kd.find_closest({5.49, 7.88, 1.12});
+	auto v = kd.find_closest({1.49, 3.88, 0.12, 2.7});
 	ASSERT_NE(nullptr, v);
-	EXPECT_EQ(Vec3(5,8,1), *v);
+	EXPECT_EQ(Vec4(1,4,0,3), *v);
 }
 
 
 TEST(Kd, FindClosestIf) {
-	Kd<double, 3> kd;
+	Kd<double, 4> kd;
 
 	insert_grid(kd);
 
-	auto v = kd.find_closest_if({5.49, 7.88, 1.12}, [](auto& x){return x[1] < 7.9;});
+	auto v = kd.find_closest_if({0.49, 3.88, 1.12, 2.8}, [](auto& x){return x[1] < 3.9;});
 	ASSERT_NE(nullptr, v);
-	EXPECT_EQ(Vec3(5,7,1), *v);
+	EXPECT_EQ(Vec4(0,3,1,3), *v);
 }
 
 
 
 TEST(Kd, FindClosestVal) {
-	Kd<double, 3, int> kd;
+	Kd<double, 4, int> kd;
 
 	insert_grid_keyval(kd);
 
-	auto v = kd.find_closest({1.12, 2.9, 6.9});
+	auto v = kd.find_closest({1.12, 2.9, 3.9, 0.8});
 	ASSERT_NE(nullptr, v);
-	EXPECT_EQ(100*1 + 10*3 + 7, v->second);
+	EXPECT_EQ(1000*1 + 100*3 + 10*4 + 1, v->second);
 }
 
 
 TEST(Kd, FindKClosest) {
-	Kd<double, 3> kd;
+	Kd<double, 4> kd;
 
 	insert_grid(kd);
 
-	auto v = kd.find_k_closest(Vec3{3.4, 5.7, 1.3}, 8);
+	auto v = kd.find_k_closest(Vec4{3.4, 3.6, 1.4, 0.1}, 8);
 	ASSERT_EQ(8, v.size());
 
-	Eigen::Matrix<double, 3, 1> r = {0,0,0};
+	Eigen::Matrix<double, 4, 1> r = {0,0,0,0};
 	for(auto& e : v) {
 		r += *e;
 	}
 	r /= v.size();
 
 	EXPECT_FLOAT_EQ(3.5, r[0]);
-	EXPECT_FLOAT_EQ(5.5, r[1]);
+	EXPECT_FLOAT_EQ(3.5, r[1]);
 	EXPECT_FLOAT_EQ(1.5, r[2]);
+	EXPECT_FLOAT_EQ(0.0, r[3]);
 }
 
 
 TEST(Kd, FindKClosestIf) {
-	Kd<double, 3> kd;
+	Kd<double, 4> kd;
 
 	insert_grid(kd);
 
-	auto v = kd.find_k_closest_if(Vec3{3.4, 5.7, 1.3}, 4, [](auto& x){return x[1] < 5.9;});
-	ASSERT_EQ(4, v.size());
+	auto v = kd.find_k_closest_if(Vec4{3.4, 3.7, 1.3, 0.3}, 8, [](auto& x){return x[1] < 3.9;});
+	ASSERT_EQ(8, v.size());
 
-	Eigen::Matrix<double, 3, 1> r = {0,0,0};
+	Eigen::Matrix<double, 4, 1> r = {0,0,0,0};
 	for(auto& e : v) {
 		r += *e;
 	}
 	r /= v.size();
 
 	EXPECT_FLOAT_EQ(3.5, r[0]);
-	EXPECT_FLOAT_EQ(5.0, r[1]);
+	EXPECT_FLOAT_EQ(3.0, r[1]);
 	EXPECT_FLOAT_EQ(1.5, r[2]);
+	EXPECT_FLOAT_EQ(0.5, r[3]);
 }
 
 
 TEST(Kd, EachAabb) {
-	Kd<double, 3> kd;
+	Kd<double, 4> kd;
 
 	insert_grid(kd);
 
-	std::vector<Vec3_noalign> v;
+	std::vector<Vec4_noalign> v;
 
-	kd.each_intersect(Eigen::AlignedBox<double, 3>(Vec3{5.5, 5.5, 5.5}, Vec3{7.5, 7.5, 7.5}), [&v](auto& e){
+	kd.each_intersect(Eigen::AlignedBox<double, 4>(Vec4{1.5, 1.5, 1.5, 1.5}, Vec4{3.5, 3.5, 3.5, 3.5}), [&v](auto& e){
 		v.emplace_back(e);
 	});
 
-	ASSERT_EQ(8, v.size());
+	ASSERT_EQ(16, v.size());
 
-	Eigen::Matrix<double, 3, 1> r = {0,0,0};
+	Eigen::Matrix<double, 4, 1> r = {0,0,0,0};
 	for(auto& e : v) {
 		r += e;
 	}
 	r /= v.size();
 
-	EXPECT_FLOAT_EQ(6.5, r[0]);
-	EXPECT_FLOAT_EQ(6.5, r[1]);
-	EXPECT_FLOAT_EQ(6.5, r[2]);
+	EXPECT_FLOAT_EQ(2.5, r[0]);
+	EXPECT_FLOAT_EQ(2.5, r[1]);
+	EXPECT_FLOAT_EQ(2.5, r[2]);
+	EXPECT_FLOAT_EQ(2.5, r[3]);
 }
 
 
